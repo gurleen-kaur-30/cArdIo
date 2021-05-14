340a205536cadbe9ae5db3d22b2958839cd40843
prateksha <pratekshau@gmail.com>
2021-05-14 21:18:20 +0530

    added more fields for signup

--- pages/admin/post.js
+++ pages/admin/post.js
@@ -1,6 +1,11 @@
 import { useState } from 'react';
 import dashify from 'dashify';
 import axios from 'axios';
+import React from 'react'
+import TextInput from '../../components/textinput'
+import styles from '../styles/login.module.scss'
+import utilStyles from '../../styles/util.module.scss'
+import DatePicker from 'react-date-picker';
 
 const Post = () => {
   const [content, setContent] = useState({
@@ -12,25 +17,49 @@ const Post = () => {
     setContent(prevState => ({ ...prevState, [name]: value }));
   }
   const onSubmit = async () => {
-    const { email, password } = content;
-    await axios.post('/api/user', { email, slug: dashify(email), password });
+    const { name, dob, email, password } = content;
+    await axios.post('/api/user', { name, email, dob, slug: dashify(email), password });
   }
   return (
-    <div>
-      <label htmlFor="email">Email</label>
-      <input
-        type="text"
-        name="email"
-        value={content.email}
-        onChange={onChange}
-      />
-      <label htmlFor="password">Password</label>
-      <textarea
-        name="password"
+    <div className={styles.box}>
+    <form className={styles.form}>
+      <p className={utilStyles.headingXl}> SIGN UP TO START YOUR FITNESS JOURNEY</p>
+     <div className={styles.innerBox}>
+     <TextInput 
+        labelName={"Name"}
+        type={"text"}
         value={content.password}
         onChange={onChange}
       />
-      <button onClick={onSubmit}>POST</button>
+      <DatePicker
+        onChange={onChange}
+        value={content.dob}
+      />
+
+      <TextInput 
+        labelName={"Email"}
+        type={"text"}
+        value={content.email}
+        onChange={onChange}
+      />
+
+      <TextInput 
+        labelName={"Password"}
+        type={"text"}
+        value={content.password}
+        onChange={onChange}
+      />
+
+      </div>
+      <div>
+      <button className={utilStyles.button} onClick={onSubmit}>
+        <text className={utilStyles.text}>
+          SIGN UP
+        </text>
+      </button>
+      </div>
+     
+    </form>
     </div>
   );
 };
