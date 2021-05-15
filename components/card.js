ae306e9def25d4e134821fb36863c42545df2bbb
gurleen-kaur-30 <gurleen.kaur@iiitb.org>
2021-05-15 21:25:54 +0530

    added links to the card

--- components/card.js
+++ components/card.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import styles from './styles/card.module.scss'
+import Link from 'next/link'
 
 export default class Card extends React.Component {
     render() {
       return(
+        <Link href={this.props.href}>
           <div className={styles.card}>
             <img className={styles.cardImage} src={this.props.image} />
             <div className={styles.cardBody}>
               <h2 className={styles.cardText}>{this.props.title}</h2>
             </div>
           </div>
+          </Link>
         )
     }
   }
--- pages/dashboard.js
+++ pages/dashboard.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Card from '../components/card.js'
 import utilStyles from '../styles/util.module.scss'
 import styles from './styles/dashboard.module.scss'
+import Link from 'next/link'
 
 export default class Dashboard extends React.Component {
     render() {
@@ -14,10 +15,12 @@ export default class Dashboard extends React.Component {
               <Card
                 image="https://i.pinimg.com/originals/8c/53/27/8c532774e4e1c524576bf1fb829ad895.gif"
                 title="Bicep Curls"
+                href="/pose"
                />
                <Card
                 image="https://177d01fbswx3jjl1t20gdr8j-wpengine.netdna-ssl.com/wp-content/uploads/2018/12/DB-Shoulder-Press.gif"
                 title="Shoulder Press"
+                href="/dashboard"
               />
               </div>
           </div>
