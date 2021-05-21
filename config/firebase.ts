81e769553b979dddec878e279cdad427f1737fe5
prateksha <pratekshau@gmail.com>
2021-05-21 13:42:50 +0530

    Changed home page

--- config/firebase.ts
+++ config/firebase.ts
@@ -23,4 +23,4 @@ const db = firebase.firestore();
 const now = firebase.firestore.Timestamp.now();
 const storage = firebase.storage();
 export { auth, db, now, storage };
-console.log(app.name ? 'Firebase Mode Activated!' : 'Firebase not working :(');
+// console.log(app.name ? 'Firebase Mode Activated!' : 'Firebase not working :(');
